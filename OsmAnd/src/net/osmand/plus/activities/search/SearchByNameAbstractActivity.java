fc31494edeed5aab65cf79ddcf5a75a77dc542e1
Victor <osmand.app@gmail.com>
2011-05-09 22:07:30 +0200

    Search countries over the country

--- OsmAnd/src/net/osmand/plus/activities/search/SearchByNameAbstractActivity.java
+++ OsmAnd/src/net/osmand/plus/activities/search/SearchByNameAbstractActivity.java
@@ -270,6 +270,10 @@ public abstract class SearchByNameAbstractActivity<T> extends OsmandListActivity
 			Collections.sort(list, cmp);
 		}
 
+		public List<T> getList() {
+			return list;
+		}
+		
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
 			View row;
